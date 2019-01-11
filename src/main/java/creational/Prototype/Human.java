6118cf83bdfd106d9471e8c5f7ca56b2b7c7a1fa
Tkachenko AI <alexfisher13@yandex.ru>
2019-01-11 15:28:43 +0300

    start write prototype

---
+++ src/main/java/creational/Prototype/Human.java
@@ -0,0 +1,43 @@
+package creational.Prototype;
+
+//В нашем классе прототипе
+// реализуем интерфейс Copyable для создания возможности прототипного копирования
+public class Human implements Copyable {
+    String name;
+    int age;
+    boolean sex;
+
+    public Human(String name, int age, boolean sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public Object copy() {
+        return null;
+    }
+}
---
+++ src/main/java/creational/Prototype/Main.java
@@ -0,0 +1,4 @@
+package creational.Prototype;
+
+public class Main {
+}
